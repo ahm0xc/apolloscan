0186caf5a221f92a34f9a14b557273a6d38ea2e2
ahm0xc <codingwebsa@gmail.com>
2025-04-29 21:27:11 +0600

    ✨ feat: add ai search fact checking

--- src/actions/server-actions.ts
+++ src/actions/server-actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { google } from '@ai-sdk/google';
+import { generateObject } from 'ai';
 import { YoutubeTranscript } from 'youtube-transcript';
 import * as z from "zod";
+
 import { extractYouTubeVideoId, isYoutubeVideoUrl } from "~/lib/utils";
 
 const CheckFactPayloadSchema = z.object({
@@ -28,18 +31,68 @@ export async function checkFact(payload: z.infer<typeof CheckFactPayloadSchema>)
         }
     }
 
-    const transcript = await YoutubeTranscript.fetchTranscript(videoId);
-    const plainTranscript = transcript.map(({ text }) => {
-        return text
-            .replace(/&amp;/g, '&')
-            .replace(/&lt;/g, '<')
-            .replace(/&gt;/g, '>')
-            .replace(/&quot;/g, '"')
-            .replace(/&#39;/g, "'")
-            .replace(/&nbsp;/g, ' ');
-    }).join(" ");
+    try {
 
-    const videoDetailsRes = await fetch(`https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`)
-    const videoDetails = await videoDetailsRes.json();
-    console.log("🚀 ~ checkFact ~ videoDetails:", videoDetails)
+        const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+        const plainTranscript = transcript.map(({ text }) => {
+            return text
+                .replace(/&amp;/g, '&')
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&quot;/g, '"')
+                .replace(/&#39;/g, "'")
+                .replace(/&nbsp;/g, ' ');
+        }).join(" ");
+
+        const videoDetailsRes = await fetch(`https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`)
+        const videoDetails = await videoDetailsRes.json();
+
+        const prompt = `I'm giving you a video. you have to check the truthfulness of the video from the web and give it a score between 1 to 100.
+         and extract claims that the video made and give it score (1-100) based on its trueness.
+         also return the sources used to check the truthfulness of the video.
+         
+         <video>
+            <title>${videoDetails.title}</title>
+            <author>${videoDetails.author_name}</author>
+            <transcript>${plainTranscript}</transcript>
+         </video>
+         `
+
+        console.log("🟡 prompt", prompt);
+
+        const { object: aiResponse } = await generateObject({
+            model: google("gemini-2.5-pro-exp-03-25", {
+                useSearchGrounding: true,
+            }),
+            schema: z.object({
+                score: z.number().min(1).max(100).describe("Truthfulness score for the video"),
+                claims: z.array(z.object({
+                    claim: z.string().describe("Claim made by the video"),
+                    score: z.number().min(1).max(100).describe("Individual Truthfulness score for the claim"),
+                })).min(1).max(10).describe("Claims made by the video"),
+                summary: z.string().max(1000).describe("Summary of the video"),
+                sources: z.array(z.string()).min(1).max(10).describe("Source urls used to check the truthfulness of the video"),
+            }),
+            prompt
+        })
+
+
+
+        console.log("🟡 aiResponse", aiResponse);
+
+        return {
+            data: {
+                score: aiResponse.score,
+                claims: aiResponse.claims,
+                summary: aiResponse.summary,
+                sources: aiResponse.sources,
+            }
+        }
+
+    } catch (error) {
+        console.error("🔴 error", error);
+        return {
+            error: "Failed to check fact",
+        }
+    }
 }
--- src/app/(dashboard)/fact-checker-form.tsx
+++ src/app/(dashboard)/fact-checker-form.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { SearchIcon } from "lucide-react";
+import { useState } from "react";
+
+import { Loader2Icon, SearchIcon } from "lucide-react";
 
 import { checkFact } from "~/actions/server-actions";
 import { Button } from "~/components/ui/button";
 
 export default function FactCheckerForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
   async function handleSubmit(ev: React.FormEvent<HTMLFormElement>) {
     ev.preventDefault();
     const formData = new FormData(ev.currentTarget);
@@ -14,11 +18,15 @@ export default function FactCheckerForm() {
       url: formData.get("url") as string,
     };
 
+    setIsLoading(true);
     const result = await checkFact(payload);
-    if (result?.error) {
+    setIsLoading(false);
+    if (result?.error || !result?.data) {
       // TODO: add toast
       console.log("🚀 ~ handleSubmit ~ error:", result.error);
     }
+
+    console.log("🚀 ~ handleSubmit ~ result:", result.data);
 
     (ev.target as HTMLFormElement).reset();
   }
@@ -34,8 +42,16 @@ export default function FactCheckerForm() {
         placeholder="Paste a video link here"
         className="bg-transparent border-none outline-none flex-1 px-4 h-full rounded-full"
       />
-      <Button size="icon" className="rounded-full bg-pink-600">
-        <SearchIcon className="w-4 h-4" />
+      <Button
+        size="icon"
+        className="rounded-full bg-pink-600"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Loader2Icon className="w-4 h-4 animate-spin" />
+        ) : (
+          <SearchIcon className="w-4 h-4" />
+        )}
       </Button>
     </form>
   );
